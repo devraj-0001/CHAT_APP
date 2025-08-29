5f16ac404966f6848f7c9c308af2e38405867b4d
dev <dev.dhanak@medront.com>
2025-08-29 21:47:22 +0530

    error fix

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -7,6 +7,7 @@ import {
   checkAuth,
 } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
+
 const router = express.Router();
 
 router.post("/signup", signup);
@@ -15,6 +16,7 @@ router.post("/logout", logout);
 
 router.put("/update-profile", protectRoute, updateProfile);
 
-// this route is use to check user , whenever we refresh the page
+// this route is used to check user whenever we refresh the page
 router.get("/check", protectRoute, checkAuth);
+
 export default router;
